264e9aa2e0645055562505512993693d6e183baf
yxxung <loveus011@naver.com>
2023-07-13 00:40:49 +0900

    유효성 검사 및 페이지 리다이렉트 완성

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,36 +1,47 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
-/*
+// 1. email 정규표현식을 사용한 validation
+const userEmail = document.getElementById("userEmail");
+const emailInput = document.querySelector(".user-email-input");
 
-1. email 정규표현식을 사용한 validation
-2. pw 정규표현식을 사용한 validation
-3. 상태 변수 관리
-4. 로그인 버튼을 클릭시 조건처리
+userEmail.addEventListener("change", (e) => {
+  emailReg(e.target.value)
+    ? emailInput.classList.remove("is--invalid")
+    : emailInput.classList.add("is--invalid");
+});
 
-*/
+// 2. pw 정규표현식을 사용한 validation
+const userPassword = document.getElementById("userPassword");
+const pwdInput = document.querySelector(".user-password-input");
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+userPassword.addEventListener("change", (e) => {
+  pwReg(e.target.value)
+    ? pwdInput.classList.remove("is--invalid")
+    : pwdInput.classList.add("is--invalid");
+});
 
-  return re.test(String(text).toLowerCase())
-}
+// 3. 상태 변수 관리
 
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+// 4. 로그인 버튼을 클릭시 조건처리
+const loginButton = document.querySelector(".btn-login");
+
+loginButton.addEventListener("click", (e) => {
+  if (userEmail.value === user.id && userPassword.value === user.pw) {
+    window.location.href = 'welcome.html'
+  }
+});
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
